725dabea2d6be45b78cf4f92dd7e342847b78128
krishnadale007 <krishnadale007@gmail.com>
2024-03-19 15:50:41 +0530

    c file

--- s3/main.tf
+++ s3/main.tf
@@ -1,14 +1,11 @@
  provider "aws" {
-     region = "us-east-1"
+     region = " var.region"
      shared_config_files      = ["/home/ubuntu/.aws/config"]
      shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
-     profile                  = "krishna"
+     profile                  = "var.profile"
 
  resource "aws_s3_bucket" "example" {
    bucket = "my-bukk-001"
 
-   tags = {
-     Name        = "My bucket"
-     Environment = "Dev"
-   }
+   tags = "var.tags"
 }
