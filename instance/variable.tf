7f13ea5672f55357a116ea52f1ddc05b03400a75
krishnadale007 <krishnadale007@gmail.com>
2024-03-19 15:27:42 +0530

    c file

---
+++ instance/variable.tf
@@ -0,0 +1,18 @@
+variable "instance_count" {}
+
+variable "image_id" {}
+
+variable "instance_type" {}
+
+variable "key_name" {}
+
+variable "project" {}
+
+variable "enviorment" {}
+
+variable "subnet_id" {}
+
+
+variable "sg_ids" {
+    type = list
+}
